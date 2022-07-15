b86fe2ad8aa24c595974a7b664cce610728bf8ae
Desu <J.lecht01@gmail.com>
2022-07-15 22:30:48 +0200

    fix: clear auth

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -41,20 +41,3 @@ def login(user: schemas.UserLoginRequest , db: Session = Depends(get_db)):
     access_token = create_acces_token(data={"user_id": str(userRecord.id)})
     return schemas.UserLoginResponse(token=access_token, tokentype="bearer")
     
-
-
-@router.post("/create/role", response_model=schemas.RoleResponse, status_code=status.HTTP_201_CREATED )
-async def create_role(role: schemas.RoleRequest, db: Session = Depends(get_db)):
-    testList = db.query(tablemodels.Role).filter(tablemodels.Role.roletype == role.roletype).first()
-    if bool(testList):
-        raise HTTPException(status_code=404, detail="to do")
-
-    testList = db.query(tablemodels.Role).filter(tablemodels.Role.roletype == role.roletype, tablemodels.Role.rolevalue == role.rolevalue).first()
-    if bool(testList):
-        raise HTTPException(status_code=404, detail="to do")
-
-    new_role= tablemodels.Role(roletype=role.roletype, rolevalue=role.rolevalue)
-    db.add(new_role)
-    db.commit()
-    db.refresh(new_role)
-    return schemas.RoleResponse(roleid=str(new_role.id))
